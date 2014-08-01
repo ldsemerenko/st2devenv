2ffbf5793852bf604bc1226cf217f043819e9e05
Dmitri Zimine <dz@stackstorm.com>
2014-07-31 23:42:48 -0700

    Cleanup and fix for npm modules - renamed poc to dev in configure.sh - deleted unused files - merged node and npm mods, else the order is not guaranteed   and we want npm/nodejs installed before trying node modules.

--- bootstrap-mods/node-modules.sh
+++ bootstrap-mods/node-modules.sh
@@ -1,9 +1,10 @@
 # Add npm modules that need to be installed to this list.
 NPM_MODULES="bower gulp"
 
-install_npm_modules() {
-    # Install npm modules.
+install_node_modules() {
+    # Install node, npm, and npm modules.
+    yum -y install npm
     npm install -g ${NPM_MODULES}
 }
 
-install_npm_modules
+install_node_modules
